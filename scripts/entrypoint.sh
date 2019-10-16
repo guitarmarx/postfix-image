aa0b90b2be94a2c77e6847e23671468318fff427
Marcus Kastner <marcuskastner@web.de>
2019-10-16 22:57:35 +0200

    added script to create lmtp mappings

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,50 +1,12 @@
 #!/bin/bash
+set -e
+
 
 #Setzen des Hostnames
 export HOSTNAME=`hostname`
 
-#Erstellen der Domänenabhängigen Konfigurationen
-if [ ! -z $DOMAIN ] && [ ! -z $KOPANO_HOST ]; then
-        echo $DOMAIN "lmtp:["${KOPANO_HOST}"]:2003" > /etc/postfix/transport
-		echo "$DOMAIN registriert"
-fi
-if [ ! -z $RELAY_DOMAIN_1 ] && [ ! -z $KOPANO_HOST_1 ]; then
-        echo $RELAY_DOMAIN_1 "lmtp:["${KOPANO_HOST_1}"]:2003" >> /etc/postfix/transport
-		echo "$RELAY_DOMAIN_1 registriert"
-fi
-if [ ! -z $RELAY_DOMAIN_2 ] && [ ! -z $KOPANO_HOST_2 ]; then
-        echo $RELAY_DOMAIN_2 "lmtp:["${KOPANO_HOST_2}"]:2003" >> /etc/postfix/transport
-		echo "$RELAY_DOMAIN_2 registriert"
-fi
-if [ ! -z $RELAY_DOMAIN_3 ] && [ ! -z $KOPANO_HOST_3 ]; then
-        echo $RELAY_DOMAIN_3 "lmtp:["${KOPANO_HOST_3}"]:2003" >> /etc/postfix/transport
-		echo "$RELAY_DOMAIN_3 registriert"
-fi
-if [ ! -z $RELAY_DOMAIN_4 ] && [ ! -z $KOPANO_HOST_4 ]; then
-        echo $RELAY_DOMAIN_4 "lmtp:["${KOPANO_HOST_4}"]:2003" >> /etc/postfix/transport
-		echo "$RELAY_DOMAIN_4 registriert"
-fi
-if [ ! -z $RELAY_DOMAIN_5 ] && [ ! -z $KOPANO_HOST_5 ]; then
-        echo $RELAY_DOMAIN_5 "lmtp:["${KOPANO_HOST_5}"]:2003" >> /etc/postfix/transport
-		echo "$RELAY_DOMAIN_5 registriert"
-fi
-if [ ! -z $RELAY_DOMAIN_6 ] && [ ! -z $KOPANO_HOST_6 ]; then
-        echo $RELAY_DOMAIN_6 "lmtp:["${KOPANO_HOST_6}"]:2003" >> /etc/postfix/transport
-		echo "$RELAY_DOMAIN_6 registriert"
-fi
-if [ ! -z $RELAY_DOMAIN_7 ] && [ ! -z $KOPANO_HOST_7 ]; then
-        echo $RELAY_DOMAIN_7 "lmtp:["${KOPANO_HOST_7}"]:2003" >> /etc/postfix/transport
-		echo "$RELAY_DOMAIN_7 registriert"
-fi
-if [ ! -z $RELAY_DOMAIN_8 ] && [ ! -z $KOPANO_HOST_8 ]; then
-        echo $RELAY_DOMAIN_8 "lmtp:["${KOPANO_HOST_8}"]:2003" >> /etc/postfix/transport
-		echo "$RELAY_DOMAIN_8 registriert"
-fi
-if [ ! -z $RELAY_DOMAIN_9 ] && [ ! -z $KOPANO_HOST_9 ]; then
-        echo $RELAY_DOMAIN_9 "lmtp:["${KOPANO_HOST_9}"]:2003" >> /etc/postfix/transport
-		echo "$RELAY_DOMAIN_9 registriert"
-fi
-
+# create lmtp mappings
+python3 /srv/scripts/createlmtpMapping.py
 postmap /etc/postfix/transport
 usermod -a -G sasl postfix
 
